4ecbaa52d3683a3ba5ca7d718964f569d2eb7c21
William Oliveira de Lagos <william.lagos@icloud.com>
2020-04-03 21:06:48 -0300

    Added first samples in Kotlin

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kotlin-coding
-Kotlin testing and learning repository 
+Kotlin testing and learning repository. To run:
+
+```
+kotlinc -script nameofscript.kts
+```
---
+++ hello.kts
@@ -0,0 +1,11 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+println("Hello, world!")
+
+var a = "Text"
+var number = 20
+
+println("$a + $number")
