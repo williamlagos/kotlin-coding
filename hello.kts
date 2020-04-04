cbaf5760864f0716cbeca01737833215ddcec65a
William Oliveira de Lagos <william.lagos@icloud.com>
2020-04-03 22:55:24 -0300

    Updated code and added source for objects

--- hello.kts
+++ hello.kts
@@ -3,9 +3,23 @@
  * play.kotlinlang.org 
  */
 
+fun printMessage(message: String) {
+    println(message)
+}
+
+fun returnValues(): Int {
+    return 0
+}
+
 println("Hello, world!")
 
 var a = "Text"
 var number = 20
 
+var months = arrayOf("January", "February", "March")
+
 println("$a + $number")
+println(months[1])
+
+printMessage(months[2])
+println(returnValues())
---
+++ objects.kts
@@ -0,0 +1,14 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+class objects {
+    fun printMessage(message: String) {
+        println(message)
+    }
+
+    fun returnValues(): Int {
+        return 0
+    }
+}
