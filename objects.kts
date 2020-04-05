241f6171248500a53bf42b4b79aad5bbf5b534c6
William Oliveira de Lagos <william.lagos@icloud.com>
2020-04-04 21:56:38 -0300

    Added some samples for objects and inheritance

--- objects.kts
+++ objects.kts
@@ -3,12 +3,35 @@
  * play.kotlinlang.org 
  */
 
-class objects {
+open class instance() {
+    open fun abc() {
+        println("Not OK")
+    }
+}
+
+class objects(var m: String, var n: Int) : instance() {
+    var msg: String
+    var number: Int
+    /*constructor(m: String, n: Int) {
+        this.msg = m
+        this.number = n    
+    }*/
+    init {
+        this.msg = m
+        this.number = n
+    }
+    override fun abc() {
+        println("OK")
+    }
     fun printMessage(message: String) {
-        println(message)
+        println(message + this.msg)
     }
 
     fun returnValues(): Int {
-        return 0
+        return this.number
     }
 }
+
+val obj = objects("abc", 0)
+obj.printMessage("ABC ")
+println(obj.returnValues())
