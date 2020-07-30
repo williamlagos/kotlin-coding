424484aa0ce7ad45bfaa8da4b8aeae12547c58f8
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-30 01:37:56 -0300

    Finished draw simple app

--- draw/app/src/main/java/io/efforia/draw/MainActivity.kt
+++ draw/app/src/main/java/io/efforia/draw/MainActivity.kt
@@ -2,10 +2,18 @@ package io.efforia.draw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun drawNumber(view: View) {
+        var text = findViewById(R.id.drawText) as TextView
+        var drewNumber = Random().nextInt(11)
+        text.setText("Número sorteado é: $drewNumber")
     }
 }
