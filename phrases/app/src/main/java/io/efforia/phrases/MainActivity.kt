287112ede2ae098c9e7dee503cd83cc7b13c62eb
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-30 02:30:06 -0300

    Added and finished phrase changing sample app

--- phrases/app/src/main/java/io/efforia/phrases/MainActivity.kt
+++ phrases/app/src/main/java/io/efforia/phrases/MainActivity.kt
@@ -2,10 +2,26 @@ package io.efforia.phrases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var text: TextView
+    val phrases = arrayOf(
+        "O importante não é vencer todos os dias, mas lutar sempre.",
+        "Maior que a tristeza de não haver vencido é a vergonha de nunca ter tentado.",
+        "É melhor conquistar a si mesmo do que vencer mil batalhas.",
+        "Enquanto houver vontade de lutar haverá esperança de vencer."
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text = findViewById(R.id.textView) as TextView
+    }
+    fun generatePhrase(view: View) {
+        val totalItemsArraySize = phrases.size
+        val randomNumber = Random().nextInt(totalItemsArraySize)
+        text.setText(phrases[randomNumber])
     }
 }
