a06e23227c428348f7275b2a86523235cfd68cd3
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-02 19:55:52 +0000

    Added company detail activity on the app

---
+++ institutional/app/src/main/java/io/efforia/institutional/DetalheEmpresaActivity.kt
@@ -0,0 +1,11 @@
+package io.efforia.institutional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheEmpresaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_empresa)
+    }
+}
