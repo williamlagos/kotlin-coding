426c61fca8a0842c0406f34ed92196e37f178529
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-02 20:03:14 +0000

    Added contact detail activity on the app

---
+++ institutional/app/src/main/java/io/efforia/institutional/DetalheContatoActivity.kt
@@ -0,0 +1,11 @@
+package io.efforia.institutional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheContatoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_contato)
+    }
+}
