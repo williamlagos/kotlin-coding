76c49a2d391fec42e2f1a31810fe97b31275eeec
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-03 17:16:29 +0000

    Updated MainActivity with onclick listeners to other activities

--- institutional/app/src/main/java/io/efforia/institutional/MainActivity.kt
+++ institutional/app/src/main/java/io/efforia/institutional/MainActivity.kt
@@ -1,11 +1,29 @@
 package io.efforia.institutional
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clientes.setOnClickListener {
+            val intent = Intent(this, DetalheClientesActivity::class.java)
+            startActivity(intent)
+        }
+        contato.setOnClickListener {
+            val intent = Intent(this, DetalheContatoActivity::class.java)
+            startActivity(intent)
+        }
+        empresa.setOnClickListener {
+            val intent = Intent(this, DetalheEmpresaActivity::class.java)
+            startActivity(intent)
+        }
+        servicos.setOnClickListener {
+            val intent = Intent(this, DetalheServicoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
