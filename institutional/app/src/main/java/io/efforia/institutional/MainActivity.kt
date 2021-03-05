868e9520e9b14a43750f11e2b0a7905a7d78aef5
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-05 18:05:26 +0000

    Added a Toasts for activity lifecycle for testing purposes

--- institutional/app/src/main/java/io/efforia/institutional/MainActivity.kt
+++ institutional/app/src/main/java/io/efforia/institutional/MainActivity.kt
@@ -28,4 +28,34 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext, "onStart", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext, "onResume", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(applicationContext, "onPause", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(applicationContext, "onStop", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(applicationContext, "onRestart", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext, "onDestroy", Toast.LENGTH_LONG).show()
+    }
 }
